d7482ca31a9740c0f4f99c41f4a1c74a2a37a3c5
Osvaldo Castillo <casdev19@gmail.com>
2023-10-31 15:50:09 -0300

    ejemplo de RestTemplate inialization

--- src/main/java/dev/castle/contentcalendar/Application.java
+++ src/main/java/dev/castle/contentcalendar/Application.java
@@ -3,14 +3,18 @@ package dev.castle.contentcalendar;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+	ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+	RestTemplate restTemplate = (RestTemplate) context.getBean("restTemplate");
+		System.out.println(restTemplate);
 	}
 
 	@Profile("dev")
@@ -20,5 +24,7 @@ public class Application {
 			System.out.println("Hello World");
 		};
 	}
+
+
 
 }
